498e28fc7618834d1f3fc328a4218593ac7a4f55
mpcodes <abuuabdul5@gmail.com>
2021-07-09 01:34:27 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react';
+
+function App() {
+  return (
+    <div className="expad">
+     <h1>Hello There "#1b2033"</h1>
+     <Button>Click Here</Button>
+    </div>
+  );
+}
+
+export default App;
