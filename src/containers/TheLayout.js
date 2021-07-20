74bde608791d83360b7ce2ebc888a04e79e4a57d
MpCodes <abuuabdul5@gmail.com>
2021-07-20 17:30:20 +0300

    changed layout

--- src/containers/TheLayout.js
+++ src/containers/TheLayout.js
@@ -1,30 +1,63 @@
 import React from 'react';
-import { TheContent, TheSidebar, TheHeader } from './index';
-import { Layout } from 'antd';
+import { Layout, Menu } from 'antd';
+import {
+	MenuUnfoldOutlined,
+	MenuFoldOutlined,
+	UserOutlined,
+	VideoCameraOutlined,
+	UploadOutlined,
+} from '@ant-design/icons';
 
-const { Sider } = Layout;
+const { Header, Sider, Content } = Layout;
 
 class TheLayout extends React.Component {
 	state = {
-		collapsed: false
+		collapsed: false,
 	};
 
-	onCollapse = (collapsed) => {
-		console.log(collapsed);
-		this.setState({ collapsed });
+	toggle = () => {
+		this.setState({
+			collapsed: !this.state.collapsed,
+		});
 	};
 
 	render() {
-		const { collapsed } = this.state;
-
 		return (
-			<Layout style={{ minHeight: '100vh' }}>
-				<Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
-					<TheSidebar />
+			<Layout>
+				<Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+					<div className='logo' />
+					<Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
+						<Menu.Item key='1' icon={<UserOutlined />}>
+							nav 1
+						</Menu.Item>
+						<Menu.Item key='2' icon={<VideoCameraOutlined />}>
+							nav 2
+						</Menu.Item>
+						<Menu.Item key='3' icon={<UploadOutlined />}>
+							nav 3
+						</Menu.Item>
+					</Menu>
 				</Sider>
 				<Layout className='site-layout'>
-					<TheHeader />
-					<TheContent />
+					<Header className='site-layout-background' style={{ padding: 0 }}>
+						{React.createElement(
+							this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+							{
+								className: 'trigger',
+								onClick: this.toggle,
+							}
+						)}
+					</Header>
+					<Content
+						className='site-layout-background'
+						style={{
+							margin: '24px 16px',
+							padding: 24,
+							minHeight: 280,
+						}}
+					>
+						Content
+					</Content>
 				</Layout>
 			</Layout>
 		);
